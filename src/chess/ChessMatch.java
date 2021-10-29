eb6b4d1029f3742d7c59cac3af96c40820a53654
Luciano Zimermann <lucianozn16@gmail.com>
2021-10-28 23:04:38 -0300

    Creating Knight class

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
@@ -273,6 +274,9 @@ public class ChessMatch
         placeNewPiece( 'a', 1, new Rook( board, Color.WHITE ) );
 		placeNewPiece( 'h', 1, new Rook( board, Color.WHITE ) );
 
+		placeNewPiece( 'b', 1, new Knight( board, Color.WHITE ) );
+		placeNewPiece( 'g', 1, new Knight( board, Color.WHITE ) );
+
 		placeNewPiece( 'c', 1, new Bishop( board, Color.WHITE ) );
 		placeNewPiece( 'f', 1, new Bishop( board, Color.WHITE ) );
 
@@ -285,6 +289,9 @@ public class ChessMatch
 
         placeNewPiece( 'a', 8, new Rook( board, Color.BLACK ) );
 		placeNewPiece( 'h', 8, new Rook( board, Color.BLACK ) );
+
+		placeNewPiece( 'b', 8, new Knight( board, Color.BLACK ) );
+		placeNewPiece( 'g', 8, new Knight( board, Color.BLACK ) );
 
 		placeNewPiece( 'c', 8, new Bishop( board, Color.BLACK ) );
 		placeNewPiece( 'f', 8, new Bishop( board, Color.BLACK ) );
